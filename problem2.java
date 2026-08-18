208fb962d897b6082d94281b2926adfa9bd14128
VATTIPALLI ANANDA <315038689+ananda-io@users.noreply.github.com>
2026-08-18 17:46:29 +0530

    add day 1 java problem

---
+++ problem2.java
@@ -0,0 +1,14 @@
+// 2nd problem  
+public class problem2{
+    public static void main(String args[])
+    {
+        System.out.println("*** JAVA FEAUTURES ***");
+        System.out.println("1. Platform independent");
+        System.out.println("2. Object Oreinted");
+        System.out.println("3. Robusts");
+        System.out.println("4. Secure");
+        System.out.println("5. Portable");
+        System.out.println("6.Multi threaded");
+        System.out.print("7.Distributed");
+    }    
+}
