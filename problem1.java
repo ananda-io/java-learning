b87325fe8de7080a0198d365821334225546a4f1
VATTIPALLI ANANDA <315038689+ananda-io@users.noreply.github.com>
2026-08-18 17:45:33 +0530

    add java problem

---
+++ problem1.java
@@ -0,0 +1,13 @@
+public class problem1 {
+    public static void main (String args[]) 
+    {
+     // 1st problem
+        System.out.println("welcome to java programming Lab");
+        System.out.println("java environment is Ready !");
+
+      
+
+    
+    }
+    
+}
