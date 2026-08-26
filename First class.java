2d008963c328fed8e349a9c1ed747b47d1690d27
VATTIPALLI ANANDA <315038689+ananda-io@users.noreply.github.com>
2026-08-26 22:40:08 +0530

    add first class

---
+++ First class.java
@@ -0,0 +1,34 @@
+class Firstclass{
+    public static void main(String args[]) {
+        System.out.println("Hello world"); // prints next line (or) we can also use \n
+        System.out.print("Hello world\n") ; 
+        System.out.print("Hello world\n");
+        System.out.println("Hello world\nfrom\napna kaksha");
+// print pattern
+//* 
+//** 
+//***     
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+// variables
+        String name = "aparna";
+        int age = 24;
+        int a = 3;
+        int b = 8;
+        double price =55.5;
+        int sum = a+b;
+        System.out.println(sum);
+        int mul = a * b;
+        System.out.println(mul);
+
+        
+
+
+
+
+
+
+    }
+}
