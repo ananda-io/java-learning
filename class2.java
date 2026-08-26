c1308014bd7078da8cb02acd2fd657adf9d36667
VATTIPALLI ANANDA <315038689+ananda-io@users.noreply.github.com>
2026-08-26 22:40:48 +0530

    add class 2

---
+++ class2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class class2
+{
+                public static void main(String[]args)
+        {
+                Scanner sc = new Scanner(System.in);
+                
+                
+                System.out.println("ENTER a : ");
+                int a = sc.nextInt();
+                System.out.println("ENTER b :");
+                int b = sc.nextInt();
+                
+                int sum = a+b;
+
+                System.out.println("sum : "+sum );
+
+        }
+
+}
