734af5e1bd5c1312be681bed14555accd2ec00c4
VATTIPALLI ANANDA <315038689+ananda-io@users.noreply.github.com>
2026-08-27 00:07:48 +0530

    add month finder

---
+++ month_finder.java
@@ -0,0 +1,60 @@
+//this is a month finder user will enter the month number and will get output the month name.
+//in this i used the concept of switch case.
+import java.util.Scanner;
+
+import javax.swing.text.AbstractDocument.BranchElement;
+public class month_finder
+{
+        public static void main(String[]args)
+        {
+                Scanner sc = new Scanner(System.in);
+                System.out.println("enter month number : ");
+                int num = sc.nextInt();
+
+                switch(num)
+                {
+                            case 1 :
+                                    System.out.println("JANUARY");
+                                    break;
+                            case 2 :
+                                    System.out.println("FEBRUARY");
+                                    break;
+                            case 3:
+                                    System.out.println("MARCH ");
+                                    break;
+                            case 4 : 
+                                    System.out.println("APRIL");
+                                    break;
+                            case 5 :
+                                    System.out.println("MAY ");
+                                    break;
+                            case 6 : 
+                                    System.out.println("JUNE ");
+                                    break;
+                            case 7 : 
+                                    System.out.println("JULY ");
+                                    break;
+                            case 8 :
+                                    System.out.println("AUGUST");
+                                    break;
+                            case 9 :
+                                    System.out.println("SEPTEMBER");
+                                    break;
+                            case 10 :
+                                    System.out.println("OCTOBER ");
+                                    break;
+                            case 11 :
+                                    System.out.println("NOVEMBER");
+                                    break;
+                            case 12 :
+                                    System.out.println("DECEMBER ");
+                                    break;
+
+                            default:
+                                    System.out.println("invalid input");
+                    
+                                
+                            
+                }
+        }
+}
