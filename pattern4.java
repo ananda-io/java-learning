4b4385cfec9eff51bb7293f582ef4bfa9670dbe4
VATTIPALLI ANANDA <315038689+ananda-io@users.noreply.github.com>
2026-08-28 20:15:27 +0530

    add inverted half pyramid

---
+++ pattern4.java
@@ -0,0 +1,31 @@
+/*
+inverted haif pyramid
+****
+***
+**
+*
+
+*/
+import java.util.*;
+public class pattern4 
+{
+    public static void main(String[]args)
+    {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("enter row size : ");
+            int n = sc.nextInt();
+
+            for(int i=n; i>=1 ; i--)
+            {
+                    for(int j=1; j<= i ; j++)
+                    {
+                            System.out.print("*");
+                    }
+
+                System.out.println();
+
+            }
+        
+    }
+}
