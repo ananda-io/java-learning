dfc1d2281b50aacb8dcbc751af53fa16b42116f3
VATTIPALLI ANANDA <315038689+ananda-io@users.noreply.github.com>
2026-08-28 20:14:21 +0530

    add half pyramid

---
+++ pattern3.java
@@ -0,0 +1,30 @@
+/*
+half pyramid
+
+*
+**
+***
+****
+
+*/
+import java .util.*;
+public class pattern3 
+{
+    public static void main(String[]args)
+    {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("enter row. size : ");
+            int n = sc.nextInt();
+
+            for(int i =1 ; i<=n ; i++)
+            {
+                    for(int j=1; j<=i ; j++)
+                    {
+                            System.out.print("*");                        
+                    }
+                
+                System.out.println();
+            }
+    }
+}
